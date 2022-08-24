b44e75dffeb9a90d6bebea0776fab4b43e01afb5
DevHars <vargassanchezfabio01@gmail.com>
2022-08-24 17:39:20 -0500

    hello word

---
+++ 00-node-fundamentals/00-hello-world/app.js
@@ -0,0 +1,19 @@
+let name = 'fabio'
+console.log(name);
+
+name = 'vargas'
+console.log(name);
+// -> fabio
+// -> vargas
+// -> undefined
+
+const name = 'fabio'
+console.log(name);
+
+name = 'vargas'
+console.log(name);
+// -> err
+// const cannot re assign values
+
+console.log('Hello Word')
+// -> Hello Word
