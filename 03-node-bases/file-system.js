4390d9742e7f64d6e7a0204ae7dcf6196e3a4fbc
DevHars <vargassanchezfabio01@gmail.com>
2022-08-31 17:46:43 -0500

    record files with file system

---
+++ 03-node-bases/file-system.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+console.clear();
+console.log('=================');
+console.log(' Table of the: 5 ');
+console.log('=================');
+console.log(' ');
+
+const base = 4;
+let output = '';
+
+for(let i = 1; i <= 10; i++) {
+    output += (`   ${base} x ${i} = ${base * i}\n`)
+}
+console.log(output);
+
+fs.writeFile( `table-${ base }.txt`, output, (err) =>{
+    if (err) throw err;
+    console.log('Table create');
+})
+
