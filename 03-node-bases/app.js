18460bfa31ed04389525a52d011fcbcea4cda013
DevHars <vargassanchezfabio01@gmail.com>
2022-08-31 17:20:49 -0500

    easy app

---
+++ 03-node-bases/app.js
@@ -0,0 +1,21 @@
+// My solution
+
+for (let i = 1; i<=10; i++) {
+    const num = 5;
+    let result;
+    console.log(`${num} x ${i} = ${result = num * i}`);
+}
+
+// Fernando 
+console.clear();
+console.log('=================');
+console.log(' Table of the: 5 ');
+console.log('=================');
+console.log(' ');
+
+const base = 5;
+for(let i = 1; i <= 10; i++) {
+    console.log(`   ${base} x ${i} = ${base * i}`)
+}
+console.log(' ');
+
