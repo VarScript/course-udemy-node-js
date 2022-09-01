c4fa9321f91c5808085036458ee6aa487af70efc
DevHars <vargassanchezfabio01@gmail.com>
2022-09-01 09:28:53 -0500

    import console files

--- 03-node-bases/import-console-files.js
+++ 03-node-bases/import-console-files.js
@@ -1,20 +1,11 @@
-const { createFile } = require('./helpers/multiply.js')
-
-// const base = 5;
-
 // Array destructuring and value assign
 const[ , , arg3 = 'base=5' ] =  process.argv;
 // Destructure of the that come of the arg3 it pass for the split with character =
 // we have two arguments for pass 
 const [ , base = 5] = arg3.split('=');
 // Print base
+console.log( base ); 
 
 
 // node import-console-files 
 // -> 5
-
-
-
-createFile( base )
-    .then( nameFile => console.log(`${nameFile} create`))
-    .catch(err => console.log(err))
