a24af976d5d6bef70a24d26db6b64170bfbe761b
DevHars <vargassanchezfabio01@gmail.com>
2022-09-06 19:49:58 -0500

    token of mapbox

--- 05-project-weather-app/models/searches.js
+++ 05-project-weather-app/models/searches.js
@@ -13,7 +13,7 @@ class Searches {
 
         try {
             // http petition
-            const answ = await axios.get('https://reqres.in/api/users?page=2');
+            const answ = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madr.json?proximity=ip&language=en&access_token=pk.eyJ1IjoidmFyc2NyaXB0IiwiYSI6ImNsN3F2YXppczA4bmUzdW12ZXNyOHQ3cWsifQ.bIy_Bmmrb02mH2J6iSSV7A');
             console.log(answ.data);
     
             return [];
