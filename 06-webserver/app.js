5360bbc1be0c885ebe1a0f75efd5c1d351cb3ae9
DevHars <vargassanchezfabio01@gmail.com>
2022-09-07 17:09:39 -0500

    req and res par 1

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -4,7 +4,17 @@ const http = require('http');
 // req : request-info solicitude / res: response-what will response to the client
 http.createServer( ( req, res ) => {
 
-    res.write('Helo world');
+
+    // Status and content type
+    res.writeHead(202, { 'Content-Type': 'application/json'});
+
+    const person = {
+        id: 1,
+        name: 'Fabio'
+    }
+
+    // Here have that be a string because it's put the JSON.stringify
+    res.write( JSON.stringify( person ));
     // For say to node that now response finalited
     res.end();
     
