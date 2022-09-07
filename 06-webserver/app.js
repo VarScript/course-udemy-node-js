1cff9d561776a0ac0f8a687c54f46b5e392b8e42
DevHars <vargassanchezfabio01@gmail.com>
2022-09-07 17:14:26 -0500

    req and res par 2

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -6,15 +6,16 @@ http.createServer( ( req, res ) => {
 
 
     // Status and content type
-    res.writeHead(202, { 'Content-Type': 'application/json'});
+    //res.writeHead(202, { 'Content-Type': 'application/json'});
+    res.setHeader('Content-Disposition', 'attachment; filename=list.csv')
+    res.writeHead(202, { 'Content-Type': 'application/cvs'});
 
-    const person = {
-        id: 1,
-        name: 'Fabio'
-    }
 
-    // Here have that be a string because it's put the JSON.stringify
-    res.write( JSON.stringify( person ));
+    res.write( 'id, name\n' );
+    res.write( '1, fabio\n' );
+    res.write( '2, maria\n' );
+    res.write( '3, juan\n' );
+
     // For say to node that now response finalited
     res.end();
     
