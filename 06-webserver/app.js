dd04db93a4f54a1f333aea3172a94f70b2c446d4
DevHars <vargassanchezfabio01@gmail.com>
2022-09-07 18:02:39 -0500

    content static serve

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,16 +2,19 @@ const express = require('express')
 const app = express();
 const port = 8080;
 
+// Content static serve of the public folder
+app.use( express.static('public') )
+
 app.get('/', (req, res) => {
     res.send('Home page')
 });
 
 app.get('/hello-word', (req, res) => {
-    res.send('Hello World in his respective rout')
+    res.send('Hello World in his respective route')
 });
 
 app.get('*', (req, res) => {
-    res.send('404 | page not found')
+    res.sendFile( __dirname + '/public/404.html')
 });
 
 app.listen(port, () => {
