feb0c093ff6cbf4c92315cc96696ce35be2a1672
DevHars <vargassanchezfabio01@gmail.com>
2022-09-07 19:07:23 -0500

    handelbars

--- 06-webserver/app-template.js
+++ 06-webserver/app-template.js
@@ -2,20 +2,27 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+app.set('view engine', 'hbs');
+
 app.use( express.static('public/template'))
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 app.get('/generic', (req, res) => {
     res.sendFile( __dirname + '/public/template/generic.html')
-})
+});
 
 app.get('/elements', (req, res) => {
     res.sendFile( __dirname + '/public/template/elements.html')
-})
+});
 
 app.get('*', (req, res) => {
     res.sendFile( __dirname + '/public/template/404.html')
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
